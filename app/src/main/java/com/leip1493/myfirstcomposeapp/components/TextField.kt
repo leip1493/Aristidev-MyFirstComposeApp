d9bc238268c08734dc7cd45b39c5a1274a5fd55f
Luis Izquierdo <leip1493@gmail.com>
2025-01-26 18:36:36 -0400

    Text fields avanzados, ejemplo de password input

--- app/src/main/java/com/leip1493/myfirstcomposeapp/components/TextField.kt
+++ app/src/main/java/com/leip1493/myfirstcomposeapp/components/TextField.kt
@@ -1,25 +1,31 @@
 package com.leip1493.myfirstcomposeapp.components
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
-import kotlin.math.max
 
 @Composable
 fun MyTextFieldParent(modifier: Modifier) {
     var input by remember { mutableStateOf("Inicializado") }
     var value by remember { mutableStateOf("") }
-
+    var password by rememberSaveable { mutableStateOf("") }
     Column(modifier) {
         MyTextField(input) { input = it }
-        MyTextField2(value){ value = it}
+        MyTextField2(value) { value = it }
+        MyAdvancedTextField(value) { value = it }
+        MyPasswordTextField(password) { password = it }
     }
 }
 
@@ -46,6 +52,39 @@ fun MyTextField2(value: String, onChange: (String) -> Unit) {
         },
         onValueChange = {
             onChange(it)
+        }
+    )
+}
+
+@Composable
+fun MyAdvancedTextField(value: String, onChange: (String) -> Unit) {
+    TextField(
+        value = value,
+        onValueChange = {
+            if (it.contains("a")) {
+                onChange(it.replace("a", "X"))
+            } else {
+                onChange(it)
+            }
+        }
+    )
+}
+
+@Composable
+fun MyPasswordTextField(password: String, onValueChange: (String) -> Unit) {
+    var passwordHidden by rememberSaveable { mutableStateOf(true) }
+
+    TextField(
+        value = password,
+        onValueChange = { onValueChange(it) },
+        singleLine = true,
+        label = { Text("Introduce tu contraseña") },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        visualTransformation = if (passwordHidden) PasswordVisualTransformation() else VisualTransformation.None,
+        trailingIcon = {
+            Text(
+                text = if (passwordHidden) "Mostrar" else "Ocultar",
+                modifier = Modifier.clickable { passwordHidden = !passwordHidden })
         }
     )
 }
