677e478825493e31e7670a4d94ec0cbc04464822
Luis Izquierdo <leip1493@gmail.com>
2025-01-26 17:29:06 -0400

    Estados avanzados: usando by y remeberSaveable

--- app/src/main/java/com/leip1493/myfirstcomposeapp/state/MyState.kt
+++ app/src/main/java/com/leip1493/myfirstcomposeapp/state/MyState.kt
@@ -3,16 +3,20 @@ package com.leip1493.myfirstcomposeapp.state
 import androidx.compose.foundation.clickable
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @Composable
 fun MyState(modifier:Modifier) {
-    val number = remember { mutableIntStateOf(0) }
+    // 1 - De esta manera no hace falta andar accediendo con .value
+    // 2 - Con el saveable podemos girar la pantalla sin perder el estado
+    var number by rememberSaveable { mutableIntStateOf(0) }
 
-    Text("Pulsame ${number.intValue}", modifier = modifier.clickable {
-        number.value += 1
+    Text("Pulsame ${number}", modifier = modifier.clickable {
+        number += 1
     })
 
 }
