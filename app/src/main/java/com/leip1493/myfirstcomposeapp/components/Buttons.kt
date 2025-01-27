a7c206e7f939175e0955683e269a8dba81079522
Luis Izquierdo <leip1493@gmail.com>
2025-01-26 21:38:04 -0400

    TextButtons

--- app/src/main/java/com/leip1493/myfirstcomposeapp/components/Buttons.kt
+++ app/src/main/java/com/leip1493/myfirstcomposeapp/components/Buttons.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -51,6 +52,10 @@ fun MyButtons(modifier: Modifier) {
             )
         ) {
             Text("Outlined")
+        }
+
+        TextButton(onClick = {}) {
+            Text("Text Button")
         }
     }
 }
