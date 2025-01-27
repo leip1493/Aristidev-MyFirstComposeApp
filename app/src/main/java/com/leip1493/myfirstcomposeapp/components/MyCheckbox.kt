19d2f97c4448cdd46f8049c318d646194db1bcee
Luis Izquierdo <leip1493@gmail.com>
2025-01-26 23:39:24 -0400

    Checkbox Tri estado (ON,OFF, Indeterminado)

--- app/src/main/java/com/leip1493/myfirstcomposeapp/components/MyCheckbox.kt
+++ app/src/main/java/com/leip1493/myfirstcomposeapp/components/MyCheckbox.kt
@@ -1,14 +1,17 @@
 package com.leip1493.myfirstcomposeapp.components
 
 import android.widget.Space
+import android.widget.ToggleButton
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TriStateCheckbox
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -17,7 +20,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.state.ToggleableState
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.VerticalAlign
 
 
 @Composable
@@ -43,9 +48,12 @@ fun MyCheckbox(modifier: Modifier) {
 
         MyCheckboxWithTextCompleted(checkInfo)
 
+        Text("Lista generada dinamicamente", modifier = Modifier.padding(start = 8.dp))
         myOptions.forEach {
             MyCheckboxWithTextCompleted(it)
         }
+
+        MyTriStatusCheckbox()
     }
 }
 
@@ -92,7 +100,6 @@ private fun MyCheckboxWithTextCompleted(checkInfo: CheckInfo) {
     }
 }
 
-
 @Composable
 fun getOptions(titles: List<String>): List<CheckInfo> {
     return titles.map {
@@ -106,4 +113,17 @@ fun getOptions(titles: List<String>): List<CheckInfo> {
     }
 }
 
-
+@Composable
+fun MyTriStatusCheckbox() {
+    var status by rememberSaveable { mutableStateOf((ToggleableState.Off)) }
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        TriStateCheckbox(state = status, onClick = {
+            status = when (status) {
+                ToggleableState.On -> ToggleableState.Off
+                ToggleableState.Off -> ToggleableState.Indeterminate
+                ToggleableState.Indeterminate -> ToggleableState.On
+            }
+        })
+        Text("Checkbox tri estado (ON, OFF, Indeterminado)")
+    }
+}
