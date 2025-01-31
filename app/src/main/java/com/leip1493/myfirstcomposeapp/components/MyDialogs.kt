4ccdd8020cbbd7024878bb4c8b672a375c450db8
Luis Izquierdo <leip1493@gmail.com>
2025-01-30 22:36:16 -0400

    Custom dialog ( imitacion de login de gmail )

--- app/src/main/java/com/leip1493/myfirstcomposeapp/components/MyDialogs.kt
+++ app/src/main/java/com/leip1493/myfirstcomposeapp/components/MyDialogs.kt
@@ -1,13 +1,17 @@
 package com.leip1493.myfirstcomposeapp.components
 
 import android.util.Log
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -19,15 +23,23 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
+import com.leip1493.myfirstcomposeapp.R
 
 @Composable
 fun MyDialogs(modifier: Modifier) {
     var showAlertDialog by remember { mutableStateOf(false) }
     var showSimpleDialog by remember { mutableStateOf(false) }
+    var showCustomDialog by remember { mutableStateOf(false) }
 
     Column(
         modifier = modifier.fillMaxSize(),
@@ -40,6 +52,10 @@ fun MyDialogs(modifier: Modifier) {
         Button(onClick = { showSimpleDialog = true }) {
             Text("Mostrar basic custom dialogo")
         }
+        Button(onClick = { showCustomDialog = true }) {
+            Text("Mostrar custom dialogo")
+        }
+
 
         MyAlertDialog(
             showAlertDialog,
@@ -51,6 +67,12 @@ fun MyDialogs(modifier: Modifier) {
         MySimpleCustomDialog(
             showSimpleDialog,
             onDismiss = { showSimpleDialog = false })
+
+
+        MyCustomDialog(
+            showCustomDialog,
+            onDismiss = { showCustomDialog = false }
+        )
     }
 }
 
@@ -87,7 +109,7 @@ fun MySimpleCustomDialog(show: Boolean, onDismiss: () -> Unit) {
 
     Dialog(
         onDismissRequest = { onDismiss() }, properties = DialogProperties(
-            dismissOnBackPress = false,
+            dismissOnBackPress = true,
             dismissOnClickOutside = false
         )
     ) {
@@ -102,4 +124,68 @@ fun MySimpleCustomDialog(show: Boolean, onDismiss: () -> Unit) {
 
     }
 
+}
+
+@Composable
+fun MyCustomDialog(show: Boolean, onDismiss: () -> Unit) {
+    if (!show) return
+    Dialog(onDismissRequest = { onDismiss() }) {
+        Column(
+            Modifier
+                .background(Color.White)
+                .padding(24.dp)
+                .fillMaxWidth()
+        ) {
+            MyCustomDialogTitle(text = "Set backup account")
+            MyCustomDialogAccountItem(
+                email = "ejemplodecorreogmaillargo@gmail.com",
+                drawable = R.drawable.avatar
+            )
+            MyCustomDialogAccountItem(
+                email = "ejemplo2@gmail.com",
+                drawable = R.drawable.avatar
+            )
+            MyCustomDialogAccountItem(
+                email = "Añadir nueva cuenta",
+                drawable = R.drawable.add
+            )
+        }
+    }
+}
+
+@Composable
+fun MyCustomDialogTitle(
+    text: String,
+) {
+    Text(
+        text,
+        color = Color.Black,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
+        modifier = Modifier.padding(bottom = 12.dp)
+    )
+}
+
+@Composable
+fun MyCustomDialogAccountItem(email: String, @DrawableRes drawable: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = painterResource(id = drawable),
+            contentDescription = "avatar",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .padding(8.dp)
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+
+        Text(
+            text = email,
+            fontSize = 14.sp,
+            color = Color.Gray,
+            modifier = Modifier.padding(8.dp),
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+    }
 }
