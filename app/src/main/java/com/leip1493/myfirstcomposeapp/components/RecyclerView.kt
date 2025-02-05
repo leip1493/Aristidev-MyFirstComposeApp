7b8cdb4dd2d265cc1a54712934b916a59ff38f1f
Luis Izquierdo <leip1493@gmail.com>
2025-02-04 23:10:33 -0400

    RecyclerView: Controlar el estado del scroll

--- app/src/main/java/com/leip1493/myfirstcomposeapp/components/RecyclerView.kt
+++ app/src/main/java/com/leip1493/myfirstcomposeapp/components/RecyclerView.kt
@@ -16,9 +16,15 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -28,6 +34,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.leip1493.myfirstcomposeapp.R
+import kotlinx.coroutines.launch
 
 data class Superhero(
     val name: String,
@@ -39,8 +46,7 @@ data class Superhero(
 @Composable
 fun SuperHeroGridView(modifier: Modifier) {
     val context = LocalContext.current
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2),
+    LazyVerticalGrid(columns = GridCells.Fixed(2),
         contentPadding = PaddingValues(16.dp), // Margen exterior al listado
         verticalArrangement = Arrangement.spacedBy(16.dp), // Espaciado vertical entre elementos
         horizontalArrangement = Arrangement.spacedBy(16.dp), // Espaciado horizontal entre elementos
@@ -51,10 +57,48 @@ fun SuperHeroGridView(modifier: Modifier) {
                     Toast.makeText(context, it.realName, Toast.LENGTH_SHORT).show()
                 }
             }
-        }
-    )
+        })
 }
 
+@Composable
+fun SuperHeroWithSpecialControlsView(modifier: Modifier) {
+    val rvState = rememberLazyListState()
+    val context = LocalContext.current
+    val coroutinesScope = rememberCoroutineScope()
+
+    Column(Modifier.fillMaxWidth()) {
+        LazyColumn(
+            state = rvState,
+            modifier = modifier.weight(1f),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            items(getSuperHeroes()) { superhero ->
+                ItemHero(superhero) {
+                    Toast.makeText(context, it.realName, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        // Esto se hace para que por cada movimiento no re-evalue la expresion y gaste cpu
+        val showButton by remember { derivedStateOf { rvState.firstVisibleItemIndex > 0 } }
+
+        if (showButton) {
+            Button(
+                onClick = {
+                    coroutinesScope.launch {
+                        rvState.animateScrollToItem(0)
+                    }
+                },
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(16.dp)
+            ) {
+                Text(text = "Soy un boton")
+            }
+        }
+
+    }
+}
 
 @Composable
 fun SuperHeroView(modifier: Modifier) {
