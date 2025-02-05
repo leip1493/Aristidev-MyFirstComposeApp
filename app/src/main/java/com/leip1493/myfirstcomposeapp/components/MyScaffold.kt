b242da4542a0b8a37d4a927be2b6473e6c58291b
Luis Izquierdo <leip1493@gmail.com>
2025-02-04 23:51:53 -0400

    Scaffold: Snackbar

--- app/src/main/java/com/leip1493/myfirstcomposeapp/components/MyScaffold.kt
+++ app/src/main/java/com/leip1493/myfirstcomposeapp/components/MyScaffold.kt
@@ -9,18 +9,27 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import kotlinx.coroutines.launch
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun MyScaffold(modifier: Modifier) {
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+
     Scaffold(
-        topBar = { MyTopAppBar() },
+        topBar = { MyTopAppBar() { coroutineScope.launch { snackbarHostState.showSnackbar("Has pulsado $it") } } },
+        snackbarHost = { SnackbarHost(snackbarHostState) },
     ) {
 
     }
@@ -28,33 +37,26 @@ fun MyScaffold(modifier: Modifier) {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MyTopAppBar() {
-    TopAppBar(
-        title = { Text("Mi primera toolbar") },
-        colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = Color.DarkGray,
-        ),
-        navigationIcon = {
-            IconButton(onClick = { /*TODO*/ }) {
-                Icon(
-                    imageVector = Icons.AutoMirrored.Outlined.ArrowBack,
-                    contentDescription = "Menu"
-                )
-            }
-        },
-        actions = {
-            IconButton(onClick = { /*TODO*/ }) {
-                Icon(
-                    imageVector = Icons.Filled.Search,
-                    contentDescription = "Menu"
-                )
-            }
-            IconButton(onClick = { /*TODO*/ }) {
-                Icon(
-                    imageVector = Icons.Filled.Close,
-                    contentDescription = "Menu"
-                )
-            }
+fun MyTopAppBar(onClickIcon: (String) -> Unit) {
+    TopAppBar(title = { Text("Mi primera toolbar") }, colors = TopAppBarDefaults.topAppBarColors(
+        containerColor = Color.DarkGray,
+    ), navigationIcon = {
+        IconButton(onClick = { onClickIcon("Atras") }) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Outlined.ArrowBack,
+                contentDescription = "Atras"
+            )
         }
-    )
+    }, actions = {
+        IconButton(onClick = { onClickIcon("Buscar") }) {
+            Icon(
+                imageVector = Icons.Filled.Search, contentDescription = "Search"
+            )
+        }
+        IconButton(onClick = { onClickIcon("Cerrar") }) {
+            Icon(
+                imageVector = Icons.Filled.Close, contentDescription = "Close"
+            )
+        }
+    })
 }
