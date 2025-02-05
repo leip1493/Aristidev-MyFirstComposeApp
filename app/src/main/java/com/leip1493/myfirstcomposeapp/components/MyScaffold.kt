9b2b80469e68ef2654b39ea8909dc2d4007cd974
Luis Izquierdo <leip1493@gmail.com>
2025-02-05 00:02:49 -0400

    Scaffold: BottomNavigationBar

--- app/src/main/java/com/leip1493/myfirstcomposeapp/components/MyScaffold.kt
+++ app/src/main/java/com/leip1493/myfirstcomposeapp/components/MyScaffold.kt
@@ -4,10 +4,16 @@ import android.annotation.SuppressLint
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.ArrowBack
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
@@ -15,8 +21,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import kotlinx.coroutines.launch
@@ -27,10 +36,9 @@ fun MyScaffold(modifier: Modifier) {
     val snackbarHostState = remember { SnackbarHostState() }
     val coroutineScope = rememberCoroutineScope()
 
-    Scaffold(
-        topBar = { MyTopAppBar() { coroutineScope.launch { snackbarHostState.showSnackbar("Has pulsado $it") } } },
+    Scaffold(topBar = { MyTopAppBar() { coroutineScope.launch { snackbarHostState.showSnackbar("Has pulsado $it") } } },
         snackbarHost = { SnackbarHost(snackbarHostState) },
-    ) {
+        bottomBar = { MyBottomNavigation() }) {
 
     }
 }
@@ -59,4 +67,30 @@ fun MyTopAppBar(onClickIcon: (String) -> Unit) {
             )
         }
     })
+}
+
+@Composable
+fun MyBottomNavigation() {
+    var index by remember { mutableStateOf(0) }
+
+    NavigationBar(containerColor = Color.DarkGray, contentColor = Color.White) {
+        NavigationBarItem(selected = index == 0, onClick = { /*TODO*/ }, icon = {
+            Icon(imageVector = Icons.Default.Home, contentDescription = "Home")
+        }, label = { Text(text = "Home") }, colors = NavigationBarItemDefaults.colors(
+            selectedIconColor = Color.Gray, indicatorColor = Color.White
+        )
+        )
+        NavigationBarItem(selected = index == 1, onClick = { /*TODO*/ }, icon = {
+            Icon(imageVector = Icons.Default.Favorite, contentDescription = "FAV")
+        }, label = { Text(text = "FAV") }, colors = NavigationBarItemDefaults.colors(
+            selectedIconColor = Color.Gray, indicatorColor = Color.White
+        )
+        )
+        NavigationBarItem(selected = index == 2, onClick = { /*TODO*/ }, icon = {
+            Icon(imageVector = Icons.Default.Person, contentDescription = "Person")
+        }, label = { Text(text = "Person") }, colors = NavigationBarItemDefaults.colors(
+            selectedIconColor = Color.Gray, indicatorColor = Color.White
+        )
+        )
+    }
 }
